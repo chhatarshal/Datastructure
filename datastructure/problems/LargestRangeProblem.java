ed4701377019a632d3d8c12a10114606e69acd57
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-09 07:22:19 +0530

    LargestRangeProblem Solution part 2 Where number need not present in adjacent position in array

--- datastructure/problems/LargestRangeProblem.java
+++ datastructure/problems/LargestRangeProblem.java
@@ -6,23 +6,31 @@ public class LargestRangeProblem {
 
 	
 	public static int[] getLartestAdjecentRange(int arr[]) {
+		System.out.println("Complexity : O(n)");
 		Arrays.sort(arr);
 		int start = 0;
 		int i = 0;
 		int count = 0;
 		int count2 = 0;
 		int start2 = 0;
-		if (arr.length < 2) {
-			return new int [] {};
+		if (arr.length == 2) {
+			return arr;
+		}
+		if (arr.length == 1) {
+			return new int[] {arr[0], arr[1]};
 		}
 		while (i < arr.length - 1) {
-			if (arr[i] + 1 == arr[i + 1]) {
+			if ((arr[i] + 1 == arr[i + 1]) || (arr[i] ==  arr[i + 1])) {
 				if (count == 0) {
 					start = i;
-				}
+				}	
 				count = count + 1;
+				if (arr[i] + 1 != arr[i + 1]) {
+					i = i + 1;
+					continue;
+				}
 			} else {
-				if ((count2 == 0 && count >0) || (count2 != -1 && count2 < count)) {
+				if (count > count2) {
 					count2 = count;
 					start2 = start;
 					count = 0;
@@ -31,15 +39,17 @@ public class LargestRangeProblem {
 			}
 			i = i + 1;
 		}		
-		System.out.println(start2);
-		System.out.println(count2 + start2);
-		return new int [] {arr[start2], arr[count2 + start2]};
+		if (count2 > count && arr[start2] != arr[count2 + start2]) {
+			return new int [] {arr[start2], arr[count2 + start2]};
+		} else {
+			return new int [] {arr[start], arr[count + start]};
+		}
 	}
 	
 	
 	
 	public static void main(String[] args) {
-		int outarr[] = LargestRangeProblem.getLartestAdjecentRange(new int [] {-8, 19});
+		int outarr[] = LargestRangeProblem.getLartestAdjecentRange(new int [] {1, 1, 1, 3, 4});
 		System.out.println(outarr.length);
 		System.out.println(outarr[0]);
 		System.out.println(outarr[1]);
