8708059ef46aeb2d71393a97bda4ba0f73a72179
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-20 23:21:17 +0530

    Depth First Search Traversal in progress....

---
+++ datastructure/search/DepthFirstSearch.java
@@ -0,0 +1,19 @@
+package com.csingh.datastructure.search;
+
+import com.csingh.datastructure.BinarySearchTree;
+
+public class DepthFirstSearch {
+	
+	public static void preOrderTraversal(BinarySearchTree bst) {
+		System.out.println("to do...");
+	}
+	
+	public static void postOrderTraversal(BinarySearchTree bst) {
+		System.out.println("to do...");
+	}
+	
+	public static void inOrderTraversal(BinarySearchTree bst) {
+		System.out.println("to do...");
+	}
+
+}
