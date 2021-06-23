781c9b4daec50f964b40229bd0374a2a859a15f4
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-23 15:47:59 +0530

    changed type from int to bigint

--- datastructure/dynamicprogramming/GridTravelerProblem.java
+++ datastructure/dynamicprogramming/GridTravelerProblem.java
@@ -1,37 +1,38 @@
 package com.csingh.datastructure.dynamicprogramming;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GridTravelerProblem {
 
 	
-	public static int gridTravelProblemSolution(int m, int n) {
-		Map<String, Integer> memo = new HashMap<>();
+	public static BigInteger gridTravelProblemSolution(int m, int n) {
+		Map<String, BigInteger> memo = new HashMap<>();
 		return helper(m, n, memo);
 	}
 	
-	public static int helper(int m, int n, Map<String, Integer> memo) {
-		Integer memoResult = memo.get(m + "#" + n);
+	public static BigInteger helper(int m, int n, Map<String, BigInteger> memo) {
+		BigInteger memoResult = memo.get(m + "#" + n);
 		if (memoResult != null) {
 			return memoResult;
 		}
 		if (m == 1 && n ==1) {
-			memo.put(m + "#" + n, 1);
-			return 1;
+			memo.put(m + "#" + n, BigInteger.ONE);
+			return BigInteger.ONE;
 		}
 		if (m < 1 || n < 1) {
-			memo.put(m + "#" + n, 0);
-			return 0;
+			memo.put(m + "#" + n, BigInteger.ZERO);
+			return BigInteger.ZERO;
 		}		
-		int result = helper(m, n - 1, memo) + helper(m - 1, n, memo);
+		BigInteger result = helper(m, n - 1, memo).add(helper(m - 1, n, memo));
 		memo.put(m + "#" + n, result);
 		return result;
 	}
 	
 	
 	public static void main(String[] args) {
-		int count = gridTravelProblemSolution(31, 31);
+		BigInteger count = gridTravelProblemSolution(31, 31);
 		System.out.println("Total ways : " + count);
 	}
 }
