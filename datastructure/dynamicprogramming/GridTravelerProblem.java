f2e997e3f78d7782f0cd4c93f77eac8efec90d44
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-23 15:42:02 +0530

    Grid Traveler problem with dynamic programming

--- datastructure/dynamicprogramming/GridTravelerProblem.java
+++ datastructure/dynamicprogramming/GridTravelerProblem.java
@@ -1,22 +1,37 @@
 package com.csingh.datastructure.dynamicprogramming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GridTravelerProblem {
 
 	
 	public static int gridTravelProblemSolution(int m, int n) {
+		Map<String, Integer> memo = new HashMap<>();
+		return helper(m, n, memo);
+	}
+	
+	public static int helper(int m, int n, Map<String, Integer> memo) {
+		Integer memoResult = memo.get(m + "#" + n);
+		if (memoResult != null) {
+			return memoResult;
+		}
 		if (m == 1 && n ==1) {
+			memo.put(m + "#" + n, 1);
 			return 1;
 		}
 		if (m < 1 || n < 1) {
+			memo.put(m + "#" + n, 0);
 			return 0;
-		}
-		
-		return gridTravelProblemSolution(m, n - 1) + gridTravelProblemSolution(m - 1, n);
+		}		
+		int result = helper(m, n - 1, memo) + helper(m - 1, n, memo);
+		memo.put(m + "#" + n, result);
+		return result;
 	}
 	
 	
 	public static void main(String[] args) {
-		int count = gridTravelProblemSolution(3, 3);
+		int count = gridTravelProblemSolution(31, 31);
 		System.out.println("Total ways : " + count);
 	}
 }
